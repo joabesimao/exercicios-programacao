b5cbb93da6099f90b539d1062eccdd2872e808a7
joabesimao <simaojoabecosta@gmail.com>
2022-12-29 13:37:22 -0300

    verifica maior numero

--- vetores/intermediario/0014-percorreArray.js
+++ vetores/intermediario/0014-percorreArray.js
@@ -3,10 +3,10 @@ const numeros = [10, 50, 80, 1, 2, 3];
 const verificaMaiorNumero = (array) => {
   let maior = 0;
 
-  for (let i = 0; i < numeros.length; i++) {
-    for (j = 1; j < numeros.length; j++) {
-      if (numeros[j] > numeros[i]) {
-        maior = numeros[j];
+  for (let i = 0; i < array.length; i++) {
+    for (j = 1; j < array.length; j++) {
+      if (array[j] > array[i]) {
+        maior = array[j];
       }
     }
   }
