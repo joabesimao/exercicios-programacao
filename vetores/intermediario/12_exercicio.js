eb309a997ad714c9ddc2113122108c5455be7b38
joabesimao <simaojoabecosta@gmail.com>
2022-12-29 08:54:36 -0300

    exercicio 13

--- vetores/intermediario/12_exercicio.js
+++ vetores/intermediario/12_exercicio.js
@@ -1,50 +1,40 @@
 /**
- * Intermediário 12. Leia 3 vetores de 9 posições 
+ * Intermediário 12. Leia 3 vetores de 9 posições
  * e crie outro com o 1º terço do primeiro,
- *  o 2º.terco do segundo 
+ *  o 2º.terco do segundo
  * e o ultimo terço do 3º.
  *  Escrever o vetor resultante ao final.
- * 
+ *
  * [1,2,3,4,5,6,7,8,9]
  * [10,20,30,40,50,60,70,80,90]
  * [100,200,300,400,500,600,700,800,900]
- * 
- * 
+ *
+ *
  * expect => [1,2,3,40,50,60,700,800,900]
- * 
- * 
+ *
+ *
  */
 
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const segundoArray = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+const terceiroArray = [100, 200, 300, 400, 500, 600, 700, 800, 900];
 
-const array = [1,2,3,4,5,6,7,8,9]
-const segundoArray = [10,20,30,40,50,60,70,80,90]
-const terceiroArray = [100,200,300,400,500,600,700,800,900]
+const separaNumeros = (array, inicioPosicao, fimPosicao) => {
+  const pegaTerco = array.slice(inicioPosicao, fimPosicao);
+  return pegaTerco;
+};
 
+const insereNoArray = (result, arrayParaInserir) => {
+  arrayParaInserir.push(...result);
+};
 
-const separaNumeros = (array,inicioPosicao,fimPosicao) =>{
-    const pegaTerco = array.slice(inicioPosicao,fimPosicao)
-    return pegaTerco
-    
-    
-    
+let arrayNovo = [];
+const inserirPrimeirosNumeros = separaNumeros(array, 0, 3);
+const inserirSegundosNumeros = separaNumeros(segundoArray, 3, 6);
+const inserirTerceiros = separaNumeros(terceiroArray, 6, 9);
 
-}
+insereNoArray(inserirPrimeirosNumeros, arrayNovo);
+insereNoArray(inserirSegundosNumeros, arrayNovo);
+insereNoArray(inserirTerceiros, arrayNovo);
 
-const insereNoArray = (result,arrayParaInserir)=>{
-    arrayParaInserir.push(...result)
-}
-
-let arrayNovo = []
-const inserirPrimeirosNumeros = separaNumeros(array,0,3)
-const inserirSegundosNumeros = separaNumeros(segundoArray,3,6)
-const inserirTerceiros = separaNumeros(terceiroArray,6,9)
-
-insereNoArray(inserirPrimeirosNumeros,arrayNovo)
-insereNoArray(inserirSegundosNumeros,arrayNovo)
-insereNoArray(inserirTerceiros,arrayNovo)
-
-
-
-
-
-console.log(arrayNovo)
+console.log(arrayNovo);
---
+++ vetores/intermediario/13_VerificaSeEhIgual.js
@@ -0,0 +1,25 @@
+/**
+ * Intermediário 13. Leia um vetor de 10 posições
+ *  e verifique se existem valores iguais e os escreva.
+ *
+ * vetor => [5,3,2,3,4,5,6,7,8,9]
+ *
+ * => 5,3
+ *
+ **/
+const listaNumeros = [5, 3, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const verificaSeONumeroEhIgual = (array) => {
+  let n = [];
+
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] === array[i]) {
+        n.push(array[i]);
+      }
+    }
+  }
+  return n;
+};
+
+console.log(verificaSeONumeroEhIgual(listaNumeros));
