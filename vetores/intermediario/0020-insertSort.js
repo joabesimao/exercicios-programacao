1b3fb815ffda0e4ec174370930aabe1b59433827
joabesimao <simaojoabecosta@gmail.com>
2023-01-04 19:00:35 -0300

    exercicio sort

--- vetores/intermediario/0020-insertSort.js
+++ vetores/intermediario/0020-insertSort.js
@@ -1,4 +1,5 @@
-let vetor = [5, 2, 4, 6, 1, 3];
+          // j  i(atual)
+let vetor = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]; // [1,2,3,4,5,6]
 
 const insertSort = (array) => {
   let tamanhoDoArray = array.length;
@@ -9,6 +10,7 @@ const insertSort = (array) => {
     let j = i - 1;
 
     while (j > -1 && atual < array[j]) {
+      //atual      = j
       array[j + 1] = array[j];
       j--;
     }
---
+++ vetores/intermediario/0021-selectSort.js
@@ -0,0 +1,23 @@
+let arrayDeNumeros = [5, 7, 8, 6, 4, 2, 1, 3, 10, 9];
+
+const selectionSort = (array) => {
+  let tamanhoDoArray = array.length;
+
+  for (let i = 0; i < tamanhoDoArray; i++) {
+    let minimo = i;
+
+    for (let j = i + 1; j < tamanhoDoArray; j++) {
+      if (array[j] < array[minimo]) {
+        minimo = j;
+      }
+    }
+    if (minimo != i) {
+      let acc = array[i];
+      array[i] = array[minimo];
+      array[minimo] = acc;
+    }
+  }
+  return array;
+};
+
+console.log(selectionSort(arrayDeNumeros));
